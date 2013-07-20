40a27ad942dadca13bb834cb45818ac4d6f00cc7
Ubuntu <ubuntu@ip-172-31-34-190.us-west-2.compute.internal>
2013-07-20 02:51:32 +0000

    updated web.js to rea from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var infile = "index.html";
+var indexhtml = fs.readFile(infile);
+var buffer = new Buffer(indexhtml,"utf-8");
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    //response.send('Hello World 2!');
+    response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
